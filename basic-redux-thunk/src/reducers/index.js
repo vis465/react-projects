d1f3d50524fbdb6544311675d52a673b76cd2f28
Vishal V <109803822+vis465@users.noreply.github.com>
2024-06-06 00:59:56 +0530

    Delete basic-redux-thunk directory

--- basic-redux-thunk/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { items, itemsHasErrored, itemsIsLoading } from './items';
-
-// Combine all reducers into root reducer
-export default combineReducers({
-    items,
-    itemsHasErrored,
-    itemsIsLoading
-});
